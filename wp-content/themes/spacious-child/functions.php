c69bfd8668362a2d865138a42f5b4602d54792c6
kcgithubcuwp <kcmisc03@yahoo.com>
2015-09-09 11:40:40 +0700

    function show how much time and memory are used
    Add function to show how much time and memory are used

--- wp-content/themes/spacious-child/functions.php
+++ wp-content/themes/spacious-child/functions.php
@@ -3,3 +3,21 @@ add_action( 'wp_enqueue_scripts', 'wpsites_load_parent_styles');
 function wpsites_load_parent_styles() {
 wp_enqueue_style( 'parent-styles', get_template_directory_uri().'/style.css' );
 }
+//  kc add just to test add info to wp_footer
+
+add_action( 'wp_footer', 'wp_footer_example' );
+ 
+function wp_footer_example() {
+    $stat = sprintf( '%d queries in %.3f seconds, using %.2fMB memory',
+        get_num_queries(),
+        timer_stop( 0, 3 ),
+        memory_get_peak_usage() / 1024 / 1024
+    );
+    if( current_user_can( 'manage_options' ) ) {
+        echo "<!-- {$stat} -->";
+    }
+}
+ 
+// Example Source: http://wordpress.stackexchange.com/a/1866
+
+?>
