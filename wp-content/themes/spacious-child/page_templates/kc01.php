3e7f9375a6367ada441c1f6bf1cc027ac0dc8936
kcgithubcuwp <kcmisc03@yahoo.com>
2015-09-15 16:18:55 +0700

    at the end just Loop through post
    No template used, see how it come out.

--- wp-content/themes/spacious-child/page_templates/kc01.php
+++ wp-content/themes/spacious-child/page_templates/kc01.php
@@ -28,6 +28,18 @@ get_header(); ?>
 		// End the loop.
 		endwhile;
 		?>
+		<?php
+ 
+                while ( have_posts() ) {
+ 
+                    the_post();
+ 
+                    the_title();
+ 
+                    the_content();
+                }
+                
+                ?>
 
 		</main><!-- .site-main -->
 	</div><!-- .content-area -->
